84e6f09032eabe34ac1e50a0a2a703fb429976e9
unisbadri <unis.badri@elementcreativestudio.com>
2021-04-27 10:20:08 +0700

    fix services

--- init-db.sh
+++ init-db.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-psql -v ON_ERROR_STOP=1 --host "127.0.0.1" --username "postgres" --dbname "postgres" --password "postgres" <<-EOSQL
+psql -v ON_ERROR_STOP=1 --host "127.0.0.1" --username "postgres" --dbname "postgres" --no-password <<-EOSQL
     CREATE USER app WITH ENCRYPTED PASSWORD 'app';
     CREATE DATABASE app;
 
